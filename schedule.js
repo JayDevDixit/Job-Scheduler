db68019be8d282de70cf1d6cb9e338dede3d80f5
Jay Dev Dixit <jaydevdixit354@gmail.com>
2026-05-31 19:26:23 +0530

    run at 2 everyday

--- schedule.js
+++ schedule.js
@@ -2,6 +2,6 @@ import {exec} from 'child_process'
 import cron from 'node-cron'
 import { scheduleJobs } from './utility.js'
 
-const cronExp = '0 6 * * *'
+const cronExp = '0 2 * * *';
 
 cron.schedule(cronExp,scheduleJobs)
